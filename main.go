2b35dd062eacd24b87cf02722fa9c7136734cc3c
FloKBCode <florence.korebelle@ynov.com>
2025-10-21 11:09:08 +0200

    debug

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
 	"power4/game"
 	"strconv"
-    "encoding/json"
-    "io/ioutil"
-    "os"
+	"time"
 )
 
 var (
@@ -17,6 +19,8 @@ var (
 	scoreP1     int
 	scoreP2     int
 	gamesPlayed int
+	aiMode      bool
+	aiDifficulty string // "facile", "moyen", "difficile"
 )
 
 type GameData struct {
@@ -25,15 +29,20 @@ type GameData struct {
 	ScoreP2      int
 	GamesPlayed  int
 	ErrorMessage string
+	AIMode       bool
+	AIDifficulty string
 }
 
+const saveFile = "power4_save.json"
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	board = game.NewBoard()
 
 	// Routes
 	http.HandleFunc("/", homePageHandler)
 	http.HandleFunc("/start", startGameHandler)
-    http.HandleFunc("/continue", continueHandler)
+	http.HandleFunc("/continue", continueHandler)
 	http.HandleFunc("/game", gameHandler)
 	http.HandleFunc("/play", playHandler)
 	http.HandleFunc("/reset", resetHandler)
@@ -48,11 +57,11 @@ func main() {
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	data := struct {
-        HasSave bool
-    }{
-        HasSave: hasSave(),
-    }
-    tmpl.ExecuteTemplate(w, "home.html", data)
+		HasSave bool
+	}{
+		HasSave: hasSave(),
+	}
+	tmpl.ExecuteTemplate(w, "home.html", data)
 }
 
 func startGameHandler(w http.ResponseWriter, r *http.Request) {
@@ -63,13 +72,19 @@ func startGameHandler(w http.ResponseWriter, r *http.Request) {
 
 	player1 := r.FormValue("player1")
 	player2 := r.FormValue("player2")
+	aiModeStr := r.FormValue("ai_mode")
+	difficulty := r.FormValue("difficulty")
 
-	// Valeurs par défaut si vides
+	// Valeurs par défaut
 	if player1 == "" {
 		player1 = "Rouge"
 	}
 	if player2 == "" {
-		player2 = "Jaune"
+		if aiModeStr == "on" {
+			player2 = "Ordinateur"
+		} else {
+			player2 = "Jaune"
+		}
 	}
 
 	// Limiter longueur
@@ -80,27 +95,41 @@ func startGameHandler(w http.ResponseWriter, r *http.Request) {
 		player2 = player2[:15]
 	}
 
-	// Nouvelle partie avec pseudos
+	// Configuration IA
+	aiMode = (aiModeStr == "on")
+	if aiMode {
+		aiDifficulty = difficulty
+		if aiDifficulty == "" {
+			aiDifficulty = "moyen"
+		}
+	}
+
+	// Nouvelle partie
 	board = game.NewBoardWithNames(player1, player2)
 	scoreP1 = 0
 	scoreP2 = 0
+	
+	// Supprimer ancienne sauvegarde
+	deleteSave()
 
 	http.Redirect(w, r, "/game", http.StatusSeeOther)
 }
 
 func continueHandler(w http.ResponseWriter, r *http.Request) {
-    if loadGame() {
-        http.Redirect(w, r, "/game", http.StatusSeeOther)
-    } else {
-        http.Redirect(w, r, "/", http.StatusSeeOther)
-    }
+	if loadGame() {
+		http.Redirect(w, r, "/game", http.StatusSeeOther)
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
 
 func gameHandler(w http.ResponseWriter, r *http.Request) {
 	data := GameData{
-		Board:   board,
-		ScoreP1: scoreP1,
-		ScoreP2: scoreP2,
+		Board:        board,
+		ScoreP1:      scoreP1,
+		ScoreP2:      scoreP2,
+		AIMode:       aiMode,
+		AIDifficulty: aiDifficulty,
 	}
 
 	err := tmpl.ExecuteTemplate(w, "game.html", data)
@@ -129,31 +158,53 @@ func playHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Coup du joueur 1
 	board.Move(col)
 	board.TotalMoves++
 	board.CheckWin()
-    saveGame()
+
+	// Sauvegarder après coup joueur
+	saveGame()
+
+	// Si mode IA, joueur 2 est actif et partie pas finie
+	if aiMode && board.Player == 2 && !board.GameOver {
+		time.Sleep(500 * time.Millisecond)
+		aiCol := getAIMove(board, aiDifficulty)
+		if aiCol != -1 {
+			board.Move(aiCol)
+			board.TotalMoves++
+			board.CheckWin()
+			
+			// Sauvegarder après coup IA
+			saveGame()
+		}
+	}
+
 	http.Redirect(w, r, "/game", http.StatusSeeOther)
 }
 
 func resetHandler(w http.ResponseWriter, r *http.Request) {
+	// Incrémenter score si partie terminée
 	if board.GameOver {
 		switch board.Winner {
-        case 1:
+		case 1:
 			scoreP1++
 		case 2:
 			scoreP2++
 		}
 		gamesPlayed++
-        deleteSave()
 	}
 
+	// Conserver pseudos
 	p1 := board.Player1Name
 	p2 := board.Player2Name
 
-
+	// Créer nouveau plateau
+	board = game.NewBoardWithNames(p1, p2)
+	
+	// Sauvegarder le nouveau plateau
 	saveGame()
-    board = game.NewBoardWithNames(p1, p2)
+
 	http.Redirect(w, r, "/game", http.StatusSeeOther)
 }
 
@@ -165,9 +216,238 @@ func resetScoresHandler(w http.ResponseWriter, r *http.Request) {
 
 	scoreP1 = 0
 	scoreP2 = 0
+	gamesPlayed = 0
 
 	http.Redirect(w, r, "/game", http.StatusSeeOther)
 }
+
+// ========== SYSTÈME IA ==========
+
+func getAIMove(b *game.Board, difficulty string) int {
+	switch difficulty {
+	case "facile":
+		return aiEasy(b)
+	case "moyen":
+		return aiMedium(b)
+	case "difficile":
+		return aiHard(b)
+	default:
+		return aiMedium(b)
+	}
+}
+
+// IA Facile : Joue au hasard
+func aiEasy(b *game.Board) int {
+	available := []int{}
+	for col := 0; col < 7; col++ {
+		if !b.IsColumnFull(col) {
+			available = append(available, col)
+		}
+	}
+	if len(available) == 0 {
+		return -1
+	}
+	return available[rand.Intn(len(available))]
+}
+
+// IA Moyen : Gagne si possible, bloque sinon joue au hasard
+func aiMedium(b *game.Board) int {
+	// 1. Essayer de gagner
+	if col := findWinningMove(b, 2); col != -1 {
+		return col
+	}
+
+	// 2. Bloquer le joueur
+	if col := findWinningMove(b, 1); col != -1 {
+		return col
+	}
+
+	// 3. Jouer au hasard
+	return aiEasy(b)
+}
+
+// IA Difficile : Stratégie avancée
+func aiHard(b *game.Board) int {
+	// 1. Gagner
+	if col := findWinningMove(b, 2); col != -1 {
+		return col
+	}
+
+	// 2. Bloquer
+	if col := findWinningMove(b, 1); col != -1 {
+		return col
+	}
+
+	// 3. Privilégier centre
+	if !b.IsColumnFull(3) {
+		return 3
+	}
+
+	// 4. Colonnes proches du centre
+	priority := []int{2, 4, 1, 5, 0, 6}
+	for _, col := range priority {
+		if !b.IsColumnFull(col) {
+			return col
+		}
+	}
+
+	return -1
+}
+
+// Trouve un coup gagnant pour le joueur spécifié
+func findWinningMove(b *game.Board, player int) int {
+	for col := 0; col < 7; col++ {
+		if b.IsColumnFull(col) {
+			continue
+		}
+
+		// Simuler le coup
+		row := simulateMove(b, col, player)
+		if row == -1 {
+			continue
+		}
+
+		// Vérifier victoire
+		if checkWinAt(b, row, col, player) {
+			// Annuler simulation
+			b.Grid[row][col] = 0
+			return col
+		}
+
+		// Annuler simulation
+		b.Grid[row][col] = 0
+	}
+	return -1
+}
+
+func simulateMove(b *game.Board, col, player int) int {
+	for row := 5; row >= 0; row-- {
+		if b.Grid[row][col] == 0 {
+			b.Grid[row][col] = player
+			return row
+		}
+	}
+	return -1
+}
+
+func checkWinAt(b *game.Board, row, col, player int) bool {
+	// Horizontal
+	count := 1
+	for c := col - 1; c >= 0 && b.Grid[row][c] == player; c-- {
+		count++
+	}
+	for c := col + 1; c < 7 && b.Grid[row][c] == player; c++ {
+		count++
+	}
+	if count >= 4 {
+		return true
+	}
+
+	// Vertical
+	count = 1
+	for r := row + 1; r < 6 && b.Grid[r][col] == player; r++ {
+		count++
+	}
+	for r := row - 1; r >= 0 && b.Grid[r][col] == player; r-- {
+		count++
+	}
+	if count >= 4 {
+		return true
+	}
+
+	// Diagonale ↗
+	count = 1
+	for i := 1; row-i >= 0 && col-i >= 0 && b.Grid[row-i][col-i] == player; i++ {
+		count++
+	}
+	for i := 1; row+i < 6 && col+i < 7 && b.Grid[row+i][col+i] == player; i++ {
+		count++
+	}
+	if count >= 4 {
+		return true
+	}
+
+	// Diagonale ↘
+	count = 1
+	for i := 1; row-i >= 0 && col+i < 7 && b.Grid[row-i][col+i] == player; i++ {
+		count++
+	}
+	for i := 1; row+i < 6 && col-i >= 0 && b.Grid[row+i][col-i] == player; i++ {
+		count++
+	}
+	return count >= 4
+}
+
+// ========== SAUVEGARDE ==========
+
+func saveGame() {
+	type SaveData struct {
+		Board        *game.Board
+		ScoreP1      int
+		ScoreP2      int
+		AIMode       bool
+		AIDifficulty string
+	}
+
+	data := SaveData{
+		Board:        board,
+		ScoreP1:      scoreP1,
+		ScoreP2:      scoreP2,
+		AIMode:       aiMode,
+		AIDifficulty: aiDifficulty,
+	}
+
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		fmt.Println("Erreur sauvegarde:", err)
+		return
+	}
+
+	err = ioutil.WriteFile(saveFile, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Erreur écriture fichier:", err)
+	}
+}
+
+func loadGame() bool {
+	data, err := ioutil.ReadFile(saveFile)
+	if err != nil {
+		return false
+	}
+
+	type SaveData struct {
+		Board        *game.Board
+		ScoreP1      int
+		ScoreP2      int
+		AIMode       bool
+		AIDifficulty string
+	}
+
+	var saveData SaveData
+	err = json.Unmarshal(data, &saveData)
+	if err != nil {
+		return false
+	}
+
+	board = saveData.Board
+	scoreP1 = saveData.ScoreP1
+	scoreP2 = saveData.ScoreP2
+	aiMode = saveData.AIMode
+	aiDifficulty = saveData.AIDifficulty
+
+	return true
+}
+
+func hasSave() bool {
+	_, err := os.Stat(saveFile)
+	return err == nil
+}
+
+func deleteSave() {
+	os.Remove(saveFile)
+}
+
+// ========== TEMPLATES ==========
 
 func init() {
 	funcMap := template.FuncMap{
@@ -198,71 +478,4 @@ func init() {
 		},
 	}
 	tmpl = template.Must(template.New("").Funcs(funcMap).ParseGlob("templates/*.html"))
-}
-
-const saveFile = "power4_save.json"
-
-
-
-// Fonction de sauvegarde
-func saveGame() {
-    type SaveData struct {
-        Board   *game.Board
-        ScoreP1 int
-        ScoreP2 int
-    }
-    
-    data := SaveData{
-        Board:   board,
-        ScoreP1: scoreP1,
-        ScoreP2: scoreP2,
-    }
-    
-    jsonData, err := json.MarshalIndent(data, "", "  ")
-    if err != nil {
-        fmt.Println("Erreur sauvegarde:", err)
-        return
-    }
-    
-    err = ioutil.WriteFile(saveFile, jsonData, 0644)
-    if err != nil {
-        fmt.Println("Erreur écriture fichier:", err)
-    }
-}
-
-// Charger la sauvegarde
-func loadGame() bool {
-    data, err := ioutil.ReadFile(saveFile)
-    if err != nil {
-        return false // Pas de sauvegarde
-    }
-    
-    type SaveData struct {
-        Board   *game.Board
-        ScoreP1 int
-        ScoreP2 int
-    }
-    
-    var saveData SaveData
-    err = json.Unmarshal(data, &saveData)
-    if err != nil {
-        return false
-    }
-    
-    board = saveData.Board
-    scoreP1 = saveData.ScoreP1
-    scoreP2 = saveData.ScoreP2
-    
-    return true
-}
-
-// Vérifier si sauvegarde existe
-func hasSave() bool {
-    _, err := os.Stat(saveFile)
-    return err == nil
-}
-
-// Supprimer la sauvegarde
-func deleteSave() {
-    os.Remove(saveFile)
 }
